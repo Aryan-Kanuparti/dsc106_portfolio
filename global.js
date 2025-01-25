3aee352d83c593c590a9e5f6c6db52b722258c74
Aryan Kanuparti <93834323+Aryan-Kanuparti@users.noreply.github.com>
2025-01-24 18:47:06 -0800

    more debug

--- global.js
+++ global.js
@@ -2,21 +2,21 @@ console.log("IT’S ALIVE!");
 
 // let basePath = window.location.hostname === 'localhost' ? '' : '/dsc106_portfolio/';
 
-let pages = [
-    { url: 'dsc106_portfolio/index.html', title: 'Home' },
-    { url: 'dsc106_portfolio/projects/index.html', title: 'Projects' },
-    { url: 'dsc106_portfolio/contact/index.html', title: 'Contact' },
-    { url: 'dsc106_portfolio/resume/index.html', title: 'Resume' },
-    { url: 'https://github.com/Aryan-Kanuparti', title: 'GitHub' },
-];
-
 // let pages = [
-//     { url: 'dsc106_portfolio/', title: 'Home' },
-//     { url: 'dsc106_portfolio/projects/', title: 'Projects' },
-//     { url: 'dsc106_portfolio/contact/', title: 'Contact' },
-//     { url: 'dsc106_portfolio/resume/', title: 'Resume' },
+//     { url: 'dsc106_portfolio/index.html', title: 'Home' },
+//     { url: 'dsc106_portfolio/projects/index.html', title: 'Projects' },
+//     { url: 'dsc106_portfolio/contact/index.html', title: 'Contact' },
+//     { url: 'dsc106_portfolio/resume/index.html', title: 'Resume' },
 //     { url: 'https://github.com/Aryan-Kanuparti', title: 'GitHub' },
-//   ];
+// ];
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: '../projects/', title: 'Projects' },
+    { url: '../contact/', title: 'Contact' },
+    { url: '../resume/', title: 'Resume' },
+    { url: 'https://github.com/Aryan-Kanuparti', title: 'GitHub' },
+  ];
   
   // home page check
   const ARE_WE_HOME = document.documentElement.classList.contains('home');
